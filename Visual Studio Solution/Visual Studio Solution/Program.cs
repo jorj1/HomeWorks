475ada44059188d671f5a03fa05e52b3b7d1ade9
jorj1 <giorgi.tsn@gmail.com>
2022-10-11 15:35:08 +0400

    First C# code

---
+++ Visual Studio Solution/Visual Studio Solution/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Visual_Studio_Solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Giorgi Tsnobiladze");
+            Console.ReadLine();
+        }
+    }
+}
